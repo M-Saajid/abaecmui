307827ffce74c59586b06f66155331f63f9fb2bb
Saajid <saajid@gapstars.net>
2022-04-15 19:36:07 +0530

    merging error

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,7 +9,6 @@ import {
 import ButtonBase from "@mui/material/ButtonBase";
 import { makeStyles, Box, styled, CssBaseline } from "@material-ui/core";
 import React from "react";
-import { border } from "@mui/system";
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
   position: "relative",
@@ -42,7 +41,7 @@ const useStyles = makeStyles((theme) =>
       height: "800px"
     },
     logo: {
-      maxWidth: 50,
+      maxWidth: 70,
       marginTop: 20,
       marginBottom: 20
     },
@@ -144,194 +143,194 @@ function HomePage() {
             </Image>
           </ImageButton>
         </Box>
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            <Box
-              mt={8}
-              container
-              display="flex"
-              direction="row"
-              alignItems="center"
-              position="relative"
-              flexWrap="wrap"
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-
-                "& > :not(style)": {
-                  width: 300,
-                  height: 500
-                }
-              }}
-            >
-              <Paper
-                className={classes.heroCards}
-                sx={{
-                  backgroundSize: "cover",
-                  marginBottom: "15px",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  backgroundImage: `url(http://st.depositphotos.com/1008939/2261/i/450/depositphotos_22619283-shopping-woman-.jpg)`
-                }}
-                elevation={24}
-              >
-                <Container
-                  sx={{
-                    marginTop: "300px",
-                    height: "60px",
-                    borderRadius: "10px",
-                    backgroundColor: "black",
-                    opacity: "0.8",
-                    color: "white"
-                  }}
-                >
-                  <Typography variant="body2" textAlign="left">
-                    Shops the collection
-                  </Typography>
-                  <Typography variant="body1">Women's</Typography>
-                </Container>
-              </Paper>
-              <Paper
-                className={classes.heroCards}
-                sx={{
-                  backgroundSize: "cover",
-                  marginBottom: "15px",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  backgroundPosition: "center",
-                  backgroundImage: `url(https://www.thefashionisto.com/wp-content/uploads/2020/11/Stylish-Male-Model-Suit-Black-Shopping-Bag-Luxury.jpg)`
-                }}
-                elevation={24}
-              >
-                <Container
-                  sx={{
-                    marginTop: "300px",
-                    height: "60px",
-                    borderRadius: "10px",
-                    backgroundColor: "black",
-                    opacity: "0.8",
-                    color: "white"
-                  }}
-                >
-                  <Typography variant="body2" textAlign="left">
-                    Shops the collection
-                  </Typography>
-                  <Typography variant="body1">Men's</Typography>
-                </Container>
-              </Paper>
-              <Paper
-                className={classes.heroCards}
-                sx={{
-                  backgroundSize: "cover",
-                  marginBottom: "15px",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  backgroundPosition: "center",
-                  backgroundImage: `url(https://i.ebayimg.com/images/g/tgIAAOSwu19erm3v/s-l400.jpg)`
-                }}
-                elevation={24}
-              >
-                <Container
-                  sx={{
-                    marginTop: "300px",
-                    height: "60px",
-                    borderRadius: "10px",
-                    backgroundColor: "black",
-                    opacity: "0.8",
-                    color: "white"
-                  }}
-                >
-                  <Typography variant="body2" textAlign="left">
-                    Shops the collection
-                  </Typography>
-                  <Typography variant="body1">Accessories</Typography>
-                </Container>
-              </Paper>
-            </Box>
-          </Grid>
-        </Grid>
-
+        {/* <Grid container spacing={8}> */}
+        {/* <Grid item xs={12}> */}
+        {/* --------------------------------------------- */}
         <Box
-          mt={20}
+          mt={8}
+          container
+          display="flex"
+          direction="row"
+          alignItems="center"
+          position="relative"
+          flexWrap="wrap"
           sx={{
+<<<<<<< HEAD
            
             backgroundColor: " rgba(184, 147, 149, 0.13)",
             marginBottom: "50px"
+=======
+            "& > :not(style)": {
+              width: 300,
+              height: 500
+            }
+>>>>>>> feature
           }}
         >
-          <Grid container spacing={3}>
-            <Grid item>
-              <Box width={400}>
-                <img
-                  width={100}
-                  src="https://static.thenounproject.com/png/2063658-200.png"
-                  alt="Free Return"
-                />
-                <Container sx={{ width: "50%" }}>
-                  <Typography variant="body1" gutterBottom fontWeight={500}>
-                    {" "}
-                    FREE RETURN
-                  </Typography>
-                  <Typography fontWeight={100} variant="subtitle2">
-                    Not what expected?Place it back on the parceland attach the
-                    bill
-                  </Typography>
-                </Container>
-              </Box>
-            </Grid>
-            <Grid item>
-              <Box width={400}>
-                <img
-                  width={100}
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxO3xI_cxS1uP6ak1QNE29z8oFyDjH4fC2UV_5C0_fuF9ntamrYwoBtCwauPq_3YVLiik&usqp=CAU"
-                  alt="Free Return"
-                />
-                <Container sx={{ width: "50%" }}>
-                  <Typography
-                    variant="body1"
-                    gutterBottom
-                    textAlign={"left"}
-                    fontWeight="500"
-                    sx={{ width: "350px" }}
-                  >
-                    {" "}
-                    SAME DAY DELIVERY
-                  </Typography>
-                  <Typography fontWeight={400} variant="subtitle2">
-                    We offera delivery services that has never been done before
-                    .Checkout today and recive your products within hours
-                  </Typography>
-                </Container>
-              </Box>
-            </Grid>
-            <Grid item>
-              <Box width={400}>
-                <img
-                  width={100}
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgtbnUq8z38w9o1g_qpUUMHwamrCKUbqxrZUWSv8ZsUj1MMjo2y31nfGRap6dn4My-JnM&usqp=CAU"
-                  alt="Free Return"
-                />
-                <Container sx={{ width: "50%" }}>
-                  <Typography
-                    variant="body1"
-                    gutterBottom
-                    textAlign={"left"}
-                    fontWeight="500"
-                    sx={{ width: "350px" }}
-                  >
-                    {" "}
-                    ALL YEAR DISCOUNT
-                  </Typography>
-                  <Typography fontWeight={400} variant="subtitle2">
-                    Looking for a deel ? You can use the code "ALL YEAR" at
-                    checkout and get money off all year around
-                  </Typography>
-                </Container>
-              </Box>
-            </Grid>
-          </Grid>
+          <Paper
+            className={classes.heroCards}
+            sx={{
+              backgroundSize: "cover",
+              marginBottom: "15px",
+              marginLeft: "auto",
+              marginRight: "auto",
+              backgroundImage: `url(http://st.depositphotos.com/1008939/2261/i/450/depositphotos_22619283-shopping-woman-.jpg)`
+            }}
+            elevation={24}
+          >
+            <Container
+              sx={{
+                marginTop: "300px",
+                height: "60px",
+                borderRadius: "10px",
+                backgroundColor: "black",
+                opacity: "0.8",
+                color: "white"
+              }}
+            >
+              <Typography variant="body2" textAlign="left">
+                Shops the collection
+              </Typography>
+              <Typography variant="body1">Women's</Typography>
+            </Container>
+          </Paper>
+          <Paper
+            className={classes.heroCards}
+            sx={{
+              backgroundSize: "cover",
+              marginBottom: "15px",
+              marginLeft: "auto",
+              marginRight: "auto",
+              backgroundPosition: "center",
+              backgroundImage: `url(https://www.thefashionisto.com/wp-content/uploads/2020/11/Stylish-Male-Model-Suit-Black-Shopping-Bag-Luxury.jpg)`
+            }}
+            elevation={24}
+          >
+            <Container
+              sx={{
+                marginTop: "300px",
+                height: "60px",
+                borderRadius: "10px",
+                backgroundColor: "black",
+                opacity: "0.8",
+                color: "white"
+              }}
+            >
+              <Typography variant="body2" textAlign="left">
+                Shops the collection
+              </Typography>
+              <Typography variant="body1">Men's</Typography>
+            </Container>
+          </Paper>
+          <Paper
+            className={classes.heroCards}
+            sx={{
+              backgroundSize: "cover",
+              marginBottom: "15px",
+              marginLeft: "auto",
+              marginRight: "auto",
+              backgroundPosition: "center",
+              backgroundImage: `url(https://i.ebayimg.com/images/g/tgIAAOSwu19erm3v/s-l400.jpg)`
+            }}
+            elevation={24}
+          >
+            <Container
+              sx={{
+                marginTop: "300px",
+                height: "60px",
+                borderRadius: "10px",
+                backgroundColor: "black",
+                opacity: "0.8",
+                color: "white"
+              }}
+            >
+              <Typography variant="body2" textAlign="left">
+                Shops the collection
+              </Typography>
+              <Typography variant="body1">Accessories</Typography>
+            </Container>
+          </Paper>
         </Box>
-        <Box mt={20} width={"100%"} height="400px" bgcolor={"rgba(184, 147, 149, 0.13)"}>
+        {/* </Grid> */}
+        {/* </Grid> */}
+        {/* ------------------------------------------------------------------------------------ */}
+        <Box
+          mt={20}
+          bgcolor="rgba(184, 147, 149, 0.13)"
+          width="100%"
+          pt={5}
+          pb={5}
+          sx={{
+            display: "flex",
+            flexWrap: "wrap"
+          }}
+        >
+          <Box ml={"auto"} mr={"auto"} mb={2}>
+            <img
+              width={100}
+              src="https://static.thenounproject.com/png/2063658-200.png"
+              alt="Free Return"
+            />
+            <Container>
+              <Typography variant="body1" gutterBottom fontWeight={500}>
+                FREE RETURN
+              </Typography>
+              <Typography
+                sx={{ width: "250px" }}
+                fontWeight={400}
+                variant="subtitle2"
+              >
+                Not what expected?Place it back on the parceland attach the bill
+              </Typography>
+            </Container>
+          </Box>
+          <Box ml={"auto"} mr={"auto"} mb={2}>
+            <img
+              width={100}
+              src="https://static.thenounproject.com/png/2063658-200.png"
+              alt="Free Return"
+            />
+            <Container>
+              <Typography variant="body1" gutterBottom fontWeight={500}>
+                SAME DAY DELIVERY
+              </Typography>
+              <Typography
+                sx={{ width: "250px" }}
+                fontWeight={400}
+                variant="subtitle2"
+              >
+                We offera delivery services that has never been done before
+                .Checkout today and recive your products within hours
+              </Typography>
+            </Container>
+          </Box>
+          <Box ml={"auto"} mr={"auto"} mb={2}>
+            <img
+              width={100}
+              src="https://static.thenounproject.com/png/2063658-200.png"
+              alt="Free Return"
+            />
+            <Container>
+              <Typography variant="body1" gutterBottom fontWeight={500}>
+                ALL YEAR DISCOUNT
+              </Typography>
+              <Typography
+                sx={{ width: "250px" }}
+                fontWeight={400}
+                variant="subtitle2"
+              >
+                Looking for a deel ? You can use the code "ALL YEAR" at checkout
+                and get money off all year around
+              </Typography>
+            </Container>
+          </Box>
+        </Box>
+        <Box
+          mt={20}
+          width={"100%"}
+          height="400px"
+          bgcolor={"rgba(184, 147, 149, 0.13)"}
+        >
           <Container fixed>
             <img
               src="https://seeklogo.com/images/P/phoenix-bird-logo-78F2C24DA2-seeklogo.com.png"
@@ -340,7 +339,7 @@ function HomePage() {
             />
           </Container>
 
-          <Grid container spacing={4}>
+          <Grid container spacing={12}>
             <Grid item xs={4}>
               <Container>
                 <Typography variant="body1" fontWeight={800}>
@@ -359,9 +358,9 @@ function HomePage() {
                   Company
                 </Typography>
                 <Stack mt={10} spacing={5}>
-                  <Typography>Watch</Typography>
-                  <Typography>Shirts</Typography>
-                  <Typography>Denium</Typography>
+                  <Typography>About us</Typography>
+                  <Typography>Press</Typography>
+                  <Typography>Careers</Typography>
                 </Stack>
               </Container>
             </Grid>
@@ -371,9 +370,9 @@ function HomePage() {
                   Services
                 </Typography>
                 <Stack mt={10} spacing={5}>
-                  <Typography>Watch</Typography>
-                  <Typography>Shirts</Typography>
-                  <Typography>Denium</Typography>
+                  <Typography>Contact</Typography>
+                  <Typography>Shipping</Typography>
+                  <Typography>Returns</Typography>
                 </Stack>
               </Container>
             </Grid>

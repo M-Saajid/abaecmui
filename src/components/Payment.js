48aff96fe39bc463763e527ec7073bf9a22d3abb
Saajid <saajid@gapstars.net>
2022-04-16 17:37:43 +0530

    initial commit

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,15 +1,31 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Divider, Stack, Typography } from "@mui/material";
 import React from "react";
 
 function Payment() {
   return (
     <Box>
-      <Container sx={{marginTop:"15px"}}>
-        <Typography fontWeight={500} variant="h4">Checkout (No of Items)</Typography>
+      <Container sx={{ marginTop: "15px" }}>
+        <Typography fontWeight={500} variant="h4">
+          Checkout (No of Items)
+        </Typography>
       </Container>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
+      <Box p={4}>
+        <Stack direction={"row"} spacing={10}>
+          <Typography variant="h6" textAlign={"left"}>
+            {" "}
+            Dilevery Address{" "}
+          </Typography>
+          <Typography variant="subtitle1"> React rd 216/1 </Typography>
+        </Stack>
+      </Box>
+      <Divider />
+      <Box>
+        <Typography variant="h6">Review Items</Typography>
+      </Box>
+      <Divider />
+      <Box>
+        <Typography variant="h6">Payment Method Card</Typography>
+      </Box>
     </Box>
   );
 }
